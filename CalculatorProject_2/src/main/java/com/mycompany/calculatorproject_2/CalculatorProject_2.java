4f9bd8ca8f61340577d1e98e25974d8e270f9ad6
Rutvi Panchal <panchrut@sheridancollege.ca>
2026-06-01 19:35:28 -0400

    Add multiplication feature

---
+++ CalculatorProject_2/src/main/java/com/mycompany/calculatorproject_2/CalculatorProject_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculatorproject_2;
+
+/**
+ *
+ * @author Rutvi Panchal
+ */
+public class CalculatorProject_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
